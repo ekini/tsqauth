aaa9b0b112d890c4675190c0d87948aff301126f
ekini <eugene@dementiev.eu>
2012-03-17 16:18:02 +0700

    Были удалены лишние import, код работает на python3. Все строки теперь переводятся в юникод, поэтому в бд имена пользователей и пароли могут иметь русские символы.
    Строки соединяются с помощью join.
    В функции delete() количество удаленных строк считается с помощью
    rowcount.

--- util/users.py
+++ util/users.py
@@ -6,26 +6,30 @@ try:
 except ImportError:
     from pysqlite2 import dbapi2 as sqlite3
 
-import argparse
 import sys # sys.exit
-import locale
+import argparse
 
-from argparse import ArgumentParser
-import ConfigParser
+try:
+    import configparser # python3
+except ImportError:
+    import ConfigParser as configparser # python2
+
 import hashlib # md5 hash
 import base64
-
 conffile = "/etc/tsqauth/config.cfg"
 cur = None
 con = None
+encoding = None
 
-def error(e):
-    print "Error:", e
+def error(*e):
+    l = list(e)
+    l.insert(0, "Error:")
+    print(" ".join(l))
 
 # выдает md5 хэш строки
 def passwd(p):
     m = hashlib.md5()
-    m.update(p)
+    m.update(p.encode(encoding))
     return base64.b64encode(m.digest())
 
 # выводит список всех пользователей с хешами паролей
@@ -34,7 +38,7 @@ def list_users():
     cur.execute("select username,password from users")
     result = cur.fetchall()
     for x, y in result:
-        print "User:", x, "md5:", y
+        print("".join(["User: ", x, " md5: ", y]))
 
 # добавляет пользователя, если такого не существует
 def add(user,password):
@@ -50,16 +54,20 @@ def add(user,password):
 def delete(user):
     global con, cur
     cur.execute("DELETE from users where username=?", (user,))
-    print cur.rowcount
+    if cur.rowcount > 0:
+        print("".join(["User \"", user, "\" deleted"]))
+    else:
+        print("".join(["User \"", user, "\" not found"]))
     con.commit()
 
 def main():
-    global con, cur
+    global con, cur, encoding
     try:
         
-        print "c: ", locale.getpreferredencoding()
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.readfp(open(conffile))
+        encoding = config.get("global", "encoding")
+
         # коннектимся к бд, создаем таблицу, если её не существует
         con = sqlite3.connect(config.get("sql_auth", "users"), 10) # timeout 10
         cur = con.cursor()
@@ -69,20 +77,23 @@ def main():
         parser.add_argument("-u", "--user", action="store")
         group = parser.add_mutually_exclusive_group()
         group.add_argument("-a", "--add", action='store_true', help="Add user")
-        group.add_argument("-l", "--list", action='store_true', help="List users")
+        group.add_argument("-l", "--list", action='store_true', help="List users (default)")
         group.add_argument("-d", "--delete", action='store_true', help="Delete user")
 
         args = parser.parse_args()
+       
+        user = args.user
+        password = args.password
 
         if args.add:
-            if (args.user is not None) and (args.password is not None):
-                add(args.user.decode(locale.getpreferredencoding()), args.password.decode(locale.getpreferredencoding()))
+            if (user is not None) and (password is not None):
+                add(user.decode(encoding).lower(), password.decode(encoding))
             else:
                 error("need username and password")
                 sys.exit(2)
         elif args.delete:
-            if (args.user is not None):
-                delete(args.user)
+            if (user is not None):
+                delete(user.decode(encoding).lower())
             else:
                 error("need username")
                 sys.exit(2)
@@ -91,9 +102,9 @@ def main():
    
 
     except IOError as e:
-        print "Cannot open config file:", e
+        error("Cannot open config file:", str(e))
     except sqlite3.OperationalError as e:
-        error(e)
+        error(str(e))
 
     except ImportError:
         pass
