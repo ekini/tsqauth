cc2cfbfe41fa94a029e34a5fe7aa925d014263ca
Eugene Dementiev <eugene@dementiev.eu>
2012-03-19 16:39:26 +0700

    Начал переделывать этот файл под общий стиль, но не успел.

--- squid/squidauth.py
+++ squid/squidauth.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python
 
-from pysqlite2 import dbapi2 as sqlite3
 import os
 import time
-import sys
+import sys # sys.exit(), sys.stdout.flush()
+import argparse
+from tsqauth import error, conffile
 
-DB_FILE = "/usr/libexec/squidldapauth/ip.db"
-log_file = "authsquid.log"
-tries = 5
+try:
+    import sqlite3
+except ImportError:
+    from pysqlite2 import dbapi2 as sqlite3
 
-con = sqlite3.connect(DB_FILE)
-cur = con.cursor()
-cur.execute("""
-        create table if not exists addresses
-        (
-          ip    varchar(15) NOT NULL,
-          user  varchar(32) NOT NULL,
-          start_time    uint(11) NOT NULL,
-          end_time      uint(11) NOT NULL
-        )
-        """)
-def failsafe_execute(query):
-    for x in range(0,tries):
-        try:
-        cur.execute(query)
-            break
-        except sqlite3.OperationalError:
-#            print ("Database is locked, retrying")
-        time.sleep(1)
-            pass 
-while (1):
-    # remove trailing characters from string
-    ip = sys.stdin.readline().strip()
+try:
+    import configparser # python3
+except ImportError:
+    import ConfigParser as configparser # python2
 
-    # delete old entries
-    failsafe_execute("delete from addresses where end_time<%s;" % time.time())
-    con.commit()
-    failsafe_execute("select * from addresses where ip=\'%s\';" % ip)
-    result = cur.fetchone()
-    if result:
-        (ip, user, start_time, end_time) = result
-    # ok reply for squid with username
-        print "OK user=%s" % user 
-    sys.stdout.flush()
-    else:
-        print "ERR"
-        sys.stdout.flush()
-cur.close()
-con.close()
+error("blah")
+
+def main():
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+    cur.execute("""
+            create table if not exists addresses
+            (
+              ip    varchar(15) NOT NULL,
+              user  varchar(32) NOT NULL,
+              start_time    uint(11) NOT NULL,
+              end_time      uint(11) NOT NULL
+            )
+            """)
+
+    while (1):
+        # remove trailing characters from string
+        ip = sys.stdin.readline().strip()
+
+        # delete old entries
+        failsafe_execute("delete from addresses where end_time<%s;" % time.time())
+        con.commit()
+        failsafe_execute("select * from addresses where ip=\'%s\';" % ip)
+        result = cur.fetchone()
+        if result:
+            (ip, user, start_time, end_time) = result
+        # ok reply for squid with username
+            print "OK user=%s" % user 
+            sys.stdout.flush()
+        else:
+            print "ERR"
+            sys.stdout.flush()
+    cur.close()
+    con.close()
+
+if __name__=="__main__":
+    main()
