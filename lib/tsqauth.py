945f5a318b5b81a5b1368bcbc792ec279cc99961
ekini <eugene@dementiev.eu>
2012-03-21 13:27:36 +0700

    Конфиг теперь читается при импорте этого модуля для инициализации переменной encoding. Добавил функцию ispy3, которая возвращает True, если текущая версия питона >= 3, getsalt, которая возвращает соль, и template, которая вытаскивает html шаблон из конфига, подставляет в результат значения, переданные в args и возвращает готовый html код.

--- lib/tsqauth.py
+++ lib/tsqauth.py
@@ -2,8 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import random
+import string
+
+try:
+    import configparser  # python3
+except ImportError:
+    import ConfigParser as configparser  # python2
 
 conffile = "/etc/tsqauth/config.cfg"
+encoding = None
+
+try:
+    config = configparser.ConfigParser()
+    config.readfp(open(conffile))
+    encoding = config.get("global", "encoding")
+except IOError as e:
+    error(str(e))
+except configparser.NoOptionError:
+    encoding = "utf-8"
 
 def error(*e):
     l = list(e)
@@ -11,5 +28,27 @@ def error(*e):
     l.append("\n")
     sys.stderr.write(" ".join(l))
 
-if __name__=="__main__":
+
+def ispy3():
+    if sys.version_info[0] >= 3:  # Python 3
+        return True
+    else:  # Python 2
+        return False
+
+
+def getsalt(chars=string.ascii_letters + string.digits):
+    # generate a random 2-character 'salt'
+    return random.choice(chars) + random.choice(chars)
+
+
+def template(name, args=dict()):
+    c = config.get("html", name)
+    template = string.Template(c)
+    return template.safe_substitute(args)
+
+
+def main():
     pass
+
+if __name__ == "__main__":
+    main()
